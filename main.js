f243cd1445ac278f6cf3889945357ac7a0605a79
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-05-18 14:51:57 +0100

    hi

--- main.js
+++ main.js
@@ -1,29 +1,45 @@
 // HEART TOGGLE FUNCTIONS
 
-let heart = document.querySelector(".favourited");
-heart.addEventListener("click", function (e) {
-  // heart.style.backgroundColor = 'var(--spot-green)'
-  heart.style.color =
-    heart.style.color === "var(--spot-green)" ? "" : "var(--spot-green)";
-  heart.style.fontWeight = heart.style.fontWeight === "bold" ? "" : "bold";
-});
+window.onload = function () {
+  fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem", {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "c1fd8eb80bmsh31fad838004364fp18f820jsn1e2bc6355511",
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  })
+    .then((response) => response.json())
+    .then((jsondata) => console.log(jsondata))
 
-let hearty = document.querySelector(".add-to-favourites");
-hearty.addEventListener("click", function (e) {
-  // heart.style.backgroundColor = 'var(--spot-green)'
-  hearty.style.color =
-    hearty.style.color === "var(--spot-green)" ? "" : "var(--spot-green)";
-  hearty.style.fontWeight = hearty.style.fontWeight === "bold" ? "" : "bold";
-});
+    .catch((err) => {
+      console.error(err);
+    });
+};
 
-// MOBILE SIDEBAR
+// let heart = document.querySelector(".favourited");
+// heart.addEventListener("click", function (e) {
+//   // heart.style.backgroundColor = 'var(--spot-green)'
+//   heart.style.color =
+//     heart.style.color === "var(--spot-green)" ? "" : "var(--spot-green)";
+//   heart.style.fontWeight = heart.style.fontWeight === "bold" ? "" : "bold";
+// });
 
-/* Open when someone clicks on the span element */
-function openNav() {
-  document.getElementById("mobileNav").style.width = "100%";
-}
+// let hearty = document.querySelector(".add-to-favourites");
+// hearty.addEventListener("click", function (e) {
+//   // heart.style.backgroundColor = 'var(--spot-green)'
+//   hearty.style.color =
+//     hearty.style.color === "var(--spot-green)" ? "" : "var(--spot-green)";
+//   hearty.style.fontWeight = hearty.style.fontWeight === "bold" ? "" : "bold";
+// });
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-  document.getElementById("mobileNav").style.width = "0%";
-}
+// // MOBILE SIDEBAR
+
+// /* Open when someone clicks on the span element */
+// function openNav() {
+//   document.getElementById("mobileNav").style.width = "100%";
+// }
+
+// /* Close when someone clicks on the "x" symbol inside the overlay */
+// function closeNav() {
+//   document.getElementById("mobileNav").style.width = "0%";
+// }
